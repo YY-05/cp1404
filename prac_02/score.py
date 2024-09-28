4a8cffc89394c686c1082c75d798a23d599718d5
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-09-28 20:50:32 +0800

    Change the structure

--- prac_02/score.py
+++ prac_02/score.py
@@ -8,18 +8,27 @@
 # else:
 #     print("Bad")
 
+import random
+
 
 def main():
-    """Get a numeric score and display its status."""
+    """Get user score, validate it, and print the result."""
     score = float(input("Enter score: "))
-    print(determine_status(score))
+
+    if score < 0 or score > 100:
+        print("Invalid score")
+    else:
+        result = determine_status(score)
+        print(result)
+
+    random_score = random.randint(0, 100)
+    random_result = determine_status(random_score)
+    print(f"The random score is {random_score} and the result of random score is {random_result}")
 
 
 def determine_status(score):
     """Determine the status of a given score."""
-    if score < 0 or score > 100:
-        return "Invalid score"
-    elif score >= 90:
+    if score >= 90:
         return "Excellent"
     elif score >= 50:
         return "Passable"
