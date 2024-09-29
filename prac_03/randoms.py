3736e6cc88c95ba5dd9d5cc673e4438a7150d591
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-09-29 19:09:36 +0800

    type

---
+++ prac_03/randoms.py
@@ -0,0 +1,12 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+# 19
+# 5 20
+print(random.randrange(3, 10, 2))  # line 2
+# 7
+# 3 9
+print(random.uniform(2.5, 5.5))  # line 3
+# 3.528031133526569
+# 2.5  5.5
+print(random.randint(1,100))
