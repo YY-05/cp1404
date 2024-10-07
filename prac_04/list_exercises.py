c9cd863a3ff5fd7b071296a5d77c9224b718adb2
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-07 13:55:10 +0800

    Woefully inadequate security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,4 +12,10 @@ print(f"The last number is {the_numbers[-1]}")
 print(f"The smallest number is {min(the_numbers)}")
 print(f"The largest of the number is {max(the_numbers)}")
 print(f"The average of the numbers is {total / NUMBER_OF_NUMBERS}")
-
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+user_input_username = input("input your username: ")
+if user_input_username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
