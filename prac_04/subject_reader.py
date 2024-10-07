8cce8e96c7ef1adb63ca2f61f00501fe1e6458b8
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-07 17:20:41 +0800

    write the whole program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -3,17 +3,45 @@ CP1404/CP5632 Practical
 Data file -> lists program
 """
 
+# FILENAME = "subject_data.txt"
+#
+#
+# def main():
+#     data = load_data()
+#     print(data)
+#
+#
+# def load_data():
+#     """Read data from file formatted like: subject,lecturer,number of students."""
+#     input_file = open(FILENAME)
+#     for line in input_file:
+#         print(line)  # See what a line looks like
+#         print(repr(line))  # See what a line really looks like
+#         line = line.strip()  # Remove the \n
+#         parts = line.split(',')  # Separate the data into its parts
+#         print(parts)  # See what the parts look like (notice the integer is a string)
+#         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+#         print(parts)  # See if that worked
+#         print("----------")
+#     input_file.close()
+#
+#
+# main()
+
+
 FILENAME = "subject_data.txt"
 
 
 def main():
     data = load_data()
     print(data)
+    display_subject_details(data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subjects = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +50,16 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        subjects.append(list(parts))
         print("----------")
     input_file.close()
+    return subjects
+
+
+def display_subject_details(subjects):
+    """Display subject details."""
+    for i in range(len(subjects)):
+        print(f"{subjects[i][0]} is taught by {subjects[i][1]} and has {subjects[i][2]} students")
 
 
 main()
