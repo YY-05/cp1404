40d595a07386dd584a23d7cf342fd17afe5d4ce6
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-17 17:08:39 +0800

    def process_records(records)

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,3 +8,15 @@ def main():
     records = get_records(FILENAME)
     champion_to_count, countries = process_records(records)
     display_results(champion_to_count, countries)
+
+
+def process_records(records):
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[INDEX_COUNTRY])
+        try:
+            champion_to_count[record[INDEX_CHAMPION]] += 1
+        except KeyError:
+            champion_to_count[record[INDEX_CHAMPION]] = 1
+    return champion_to_count, countries
