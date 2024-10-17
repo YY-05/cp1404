8f6a11509d06edc21958ab0bc4e0f1d5711e83a5
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-17 16:21:54 +0800

    for

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,1 +1,9 @@
 texts = input("Text: ")
+words = texts.split()
+word_counts = {}
+for word in words:
+    if word in word_counts:
+        word_counts[word] += 1
+    else:
+        word_counts[word] = 1
+        
