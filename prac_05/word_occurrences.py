34ee434d569792fe77451db38ae29498c5919363
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-17 16:23:58 +0800

    for

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,3 +7,6 @@ for word in words:
     else:
         word_counts[word] = 1
 width = max(len(word) for word in word_counts)
+sorted_words = sorted(word_counts.keys())
+for word in sorted_words:
+    print(f"{word:{width}} : {word_counts[word]}")
