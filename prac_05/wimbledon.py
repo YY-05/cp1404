8019b1e15c691fb90501f9f412fb66070e1f31bb
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-17 18:21:39 +0800

    def get_records(filename):

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -21,3 +21,23 @@ def process_records(records):
         except KeyError:
             champion_to_count[record[INDEX_CHAMPION]] = 1
     return champion_to_count, countries
+
+
+def display_results(champion_to_count, countries):
+    print("Wimbledon Champions: ")
+    for name, count in champion_to_count.items():
+        print(name, count)
+    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(country for country in sorted(countries)))
+
+
+def get_records(filename):
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Remove header
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    return records
+
+main()
