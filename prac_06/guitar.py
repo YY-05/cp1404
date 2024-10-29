072c0f615e4b30d77844a3a150831508553c76f7
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-29 10:31:58 +0800

    Docstring

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,7 +2,9 @@ class Guitar:
     """Guitar class for storing details of a guitar."""
 
     def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar."""
         self.name = name
         self.year = year
         self.cost = cost
+
 
