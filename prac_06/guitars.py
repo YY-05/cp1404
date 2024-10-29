e3569dce18e23da7e3122f852f7f90265f60d2e8
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-29 11:15:12 +0800

    sort

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -17,5 +17,5 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-
+    guitars.sort(key=lambda x: x.name)
 main()
