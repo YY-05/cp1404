b68eb4c0e63db2bb4bc6881e6c3034d89f2b0ce5
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-29 11:22:14 +0800

    print and docstring

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,6 +2,7 @@ from prac_06.guitar import Guitar
 
 
 def main():
+    """Guitar program, using Guitar class."""
     guitars = []
 
     print("My guitars!")
@@ -18,4 +19,14 @@ def main():
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     guitars.sort(key=lambda x: x.name)
+
+    if guitars:
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = " (vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print("No guitars :( Quick, go and buy one!")
+
+
 main()
