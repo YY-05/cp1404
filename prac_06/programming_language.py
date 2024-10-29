d7504177d42153cf228b17b7642478230e532c49
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-29 10:03:50 +0800

    __str__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -11,3 +11,7 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
+
+    def __str__(self):
+        """Return string representation of a ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
