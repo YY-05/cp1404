1106b4addac98d135816db5e976f3b7b1009c023
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-29 09:29:24 +0800

    __init__

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,2 +1,8 @@
 class ProgrammingLanguage:
     """Represent information about a programming language."""
+
+    def __int__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
