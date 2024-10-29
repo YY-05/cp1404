ad80e14aac6750cbe7765b63ede31d9bfcde4de0
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-29 10:44:43 +0800

    docstring

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -8,5 +8,6 @@ class Guitar:
         self.cost = cost
 
     def __str__(self):
+        """Return a string representation of a Guitar."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
