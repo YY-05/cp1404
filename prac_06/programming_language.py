dd6beaeaf31e64823a7ab924a84205176cf88b32
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-29 09:30:55 +0800

    Docstring

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,6 +2,7 @@ class ProgrammingLanguage:
     """Represent information about a programming language."""
 
     def __int__(self, name, typing, reflection, year):
+        """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
