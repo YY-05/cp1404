59edd15e79ff547dae193ab048fd600df0840e3c
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-29 11:13:57 +0800

    got input

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,1 +1,18 @@
 from prac_06.guitar import Guitar
+
+
+def main():
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(f"{guitar_to_add.name} ({guitar_to_add.year}) : ${guitar_to_add.cost:.2f} added.")
+        name = input("Name: ")
+
+
+main()
