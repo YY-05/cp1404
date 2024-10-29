80a377e2c11af932f2594009248c37bc235463f5
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-29 10:31:17 +0800

    def __int__

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,2 +1,8 @@
 class Guitar:
     """Guitar class for storing details of a guitar."""
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
