39b3033129259cb3bfc1551f7f15a5ab7995926a
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-10-29 11:14:45 +0800

    Add the pre-defined guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -14,5 +14,8 @@ def main():
         print(f"{guitar_to_add.name} ({guitar_to_add.year}) : ${guitar_to_add.cost:.2f} added.")
         name = input("Name: ")
 
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
 
 main()
