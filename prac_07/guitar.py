78cf9b484feb2e9ad70ffc2c0b0b3dfaacd77590
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-04 17:21:22 +0800

    __lt__

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -22,3 +22,7 @@ class Guitar:
     def is_vintage(self):
         """Determine if a Guitar is considered vintage or not based on age."""
         return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        """Define less than (<) based on the year of the guitar for sorting."""
+        return self.year < other.year
