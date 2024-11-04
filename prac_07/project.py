9c0fe22c4b50e5d67c819ba41c807ae9ff4ec614
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-04 17:43:44 +0800

    def __lt__(self, other)

--- prac_07/project.py
+++ prac_07/project.py
@@ -15,3 +15,7 @@ class Project:
     def __repr__(self):
         """return a string"""
         return f"{self.name}, start: {self.start_date}, priority: {self.priority}, estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
+
+    def __lt__(self, other):
+        """make the project is sorting by priority"""
+        return self.priority < other.priority
