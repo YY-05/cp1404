64a6fca89f64db59a1d8dd2244da1b8ebbeb23bb
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-04 17:34:17 +0800

    def __init__

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,3 +3,11 @@ import datetime
 
 class Project:
     """Represent information about a Project."""
+
+    def __init__(self, name, typing, reflection, pointer_arithmetic, year):
+        """Construct a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.pointer_arithmetic = pointer_arithmetic
+        self.year = year
