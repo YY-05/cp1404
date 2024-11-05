acd38f797090190fd4843251fefe486c5e2df5eb
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-05 10:55:23 +0800

    add a new project to memory

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -48,3 +48,4 @@ def add_new_project(projects):
     priority = int(input("Priority: "))
     cost_estimate = float(input("Cost estimate: $"))
     completion_percentage = int(input("Percent complete: "))
+    projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
