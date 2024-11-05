aa62d88a4ac38d5843f082ccdde41381b477dbb0
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-05 09:08:25 +0800

    Load projects function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,2 +1,13 @@
 from project import Project
 import datetime
+
+
+def load_projects(filename):
+    projects = []
+    in_file = open(filename, "r")
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split("\t")
+        projects.append(Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4])))
+    in_file.close()
+    return projects
