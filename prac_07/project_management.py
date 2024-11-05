1e3bb23b52b54d80cfd0eb06febda75238643694
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-05 11:08:40 +0800

    print welcome message

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -6,6 +6,7 @@ def main():
     filename = "projects.txt"
     menu = """- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date
     - (A)dd new project)\n- (U)pdate project\n- (Q)uit"""
+    print("Welcome to Pythonic Project Management")
 
 
 def load_projects(filename):
