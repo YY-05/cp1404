040e860d915a9f6951e79722fc370cdb22f61b52
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-14 14:56:41 +0800

    run the whole program and check output

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -8,43 +8,35 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
-__author__ = 'Lindsay Ward'
-
-MILES_TO_KM = 1.60934
+FACTOR_MILES_TO_KM = 1.60934
 
 
 class MilesConverterApp(App):
-    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+    """Kivy App for converting miles to kilometres."""
     output_km = StringProperty()
 
     def build(self):
-        """ Build the Kivy app from the kv file """
+        """Build the Kivy app from the kv file."""
         self.title = "Convert Miles to Kilometres"
-        try:
-            self.root = Builder.load_file('convert_miles_km.kv')
-        except FileNotFoundError:
-            print("Error: The .kv file 'convert_miles_km.kv' was not found.")
-            return None
+        self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_calculate(self, text):
-        """ Handle calculation, output result to label widget """
+        """Handle calculation (could be button press or other call)."""
+        print("handle calculate")
         miles = self.convert_to_number(text)
         self.update_result(miles)
 
     def handle_increment(self, text, change):
-        """
-        Handle up/down button press, update the text input with new value, call calculation function
-        :param text: current text in the input field
-        :param change: the amount to change the miles by
-        """
+        """Handle up/down button press, update the text input with new value, call calculation function."""
+        print("handle increment")
         miles = self.convert_to_number(text) + change
         self.root.ids.input_miles.text = str(miles)
-        self.update_result(miles)
+        # Since the InputText.text has changed, its on_text event will fire and handle_calculate will be called
 
     def update_result(self, miles):
-        """ Update the output label with kilometers value """
-        self.output_km = str(miles * MILES_TO_KM)
+        print("update")
+        self.output_km = str(miles * FACTOR_MILES_TO_KM)
 
     @staticmethod
     def convert_to_number(text):
