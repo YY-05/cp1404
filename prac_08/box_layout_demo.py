9ec7e4482ef55585b8f75a06000bd674d81384e9
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-14 17:57:44 +0800

    def handle_great(self)

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,5 +8,10 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_great(self):
+        print("test")
+        self.root.ids.output_label.text = "Hello "
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
 
 BoxLayoutDemo().run()
