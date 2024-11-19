6828a7327824644851d6c372d023a14e923e16d5
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-19 09:46:17 +0800

    display the distance they drove

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -5,3 +5,8 @@ def main():
 
     good_car = UnreliableCar("Mostly Good", 100, 90)
     bad_car = UnreliableCar("Dodgy", 100, 9)
+
+    for i in range(1, 12):
+        print(f"Attempting to drive {i}km:")
+        print(f"{good_car.name:12} drove {good_car.drive(i):2}km")
+        print(f"{bad_car.name:12} drove {bad_car.drive(i):2}km")
