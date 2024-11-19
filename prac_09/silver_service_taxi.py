f8e1d27cc0e660d08d7950e31e799e66af4786e3
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-19 09:54:18 +0800

    def get_fare

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -12,3 +12,6 @@ class SilverServiceTaxi(Taxi):
     def __str__(self):
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
 
+    def get_fare(self):
+        return self.flagfall + super().get_fare()
+
