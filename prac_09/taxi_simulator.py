62821e3e31a47a6e74a4bc386c966a42a39baa98
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-19 10:51:56 +0800

    docstrings

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -6,6 +6,12 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
+    """Write a taxi simulator program that uses your Taxi and SilverServiceTaxi
+    classes. Each time, until they quit:
+    The user should be presented with a list of available taxis and get to
+    choose one. Then they should say how far they want to drive.
+    At the end of each trip, show them the price and add it to their bill.
+    """
     total_bill = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
@@ -17,6 +23,7 @@ def main():
         if menu_choice == "c":
             print("Taxis available: ")
             display_taxis(taxis)
+            # no error-checking
             taxi_choice = int(input("Choose taxi: "))
             try:
                 current_taxi = taxis[taxi_choice]
@@ -37,6 +44,7 @@ def main():
         print("Bill to date: ${total_bill:.2f}")
         print(MENU)
         menu_choice = input(">>> ").lower()
+
     print(f"Total trip cost: ${total_bill:.2f}")
     print("Taxis are now:")
     display_taxis(taxis)
@@ -49,6 +57,7 @@ def display_taxis(taxis):
 
 
 def run_tests():
+    """Run tests to show workings of Car and Taxi classes."""
     bus = Car("Datsun", 180)
     bus.drive(30)
     print("fuel =", bus.fuel)
