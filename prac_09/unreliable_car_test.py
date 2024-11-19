5fce3d7fec70970e7580336d5114ace93c788844
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-19 09:47:18 +0800

    docstrings

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -2,6 +2,7 @@ from prac_09.unreliable_car import UnreliableCar
 
 
 def main():
+    """Test some UnreliableCars."""
 
     good_car = UnreliableCar("Mostly Good", 100, 90)
     bad_car = UnreliableCar("Dodgy", 100, 9)
