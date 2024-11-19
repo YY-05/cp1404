8e988ae1f8ade76d03d56654f0474b418fa4be7e
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-19 10:29:54 +0800

    def __str__

--- prac_09/band.py
+++ prac_09/band.py
@@ -3,6 +3,9 @@ class Band:
         self.name = name
         self.musicians = []
 
+    def __str__(self):
+        return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
+
     def add(self, musician):
         self.musicians.append(musician)
 
