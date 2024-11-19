3bf8a5eef5ef2dbe0a10095789ed286bcba1a9b4
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-19 09:50:19 +0800

    Add a new attribute

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,10 @@
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
