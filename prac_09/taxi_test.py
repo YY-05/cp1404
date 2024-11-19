bfbf92071f13b5cc301f073a6f5d494982166a0a
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-19 09:03:26 +0800

    docstrings

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,7 +2,8 @@ from prac_09.taxi import Taxi
 
 
 def main():
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    """Test Taxi class."""
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
