a3eadb90490c393b61e84ccd1d25081028686181
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-19 10:49:07 +0800

    def run tests

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -48,4 +48,34 @@ def display_taxis(taxis):
         print(f"{i} - {taxi}")
 
 
+def run_tests():
+    bus = Car("Datsun", 180)
+    bus.drive(30)
+    print("fuel =", bus.fuel)
+    print("odo =", bus._odometer)
+    bus.drive(55)
+    print("fuel =", bus.fuel)
+    print("odo = ", bus._odometer)
+    print(bus)
+
+    distance = int(input("Drive how far? "))
+    while distance > 0:
+        distance_travelled = bus.drive(distance)
+        print(f"{bus} travelled {distance_travelled}")
+        distance = int(input("Drive how far? "))
+
+    t = Taxi("Prius 1", 100)
+    print(t)
+    t.drive(25)
+    print(t, t.get_fare())
+    t.start_fare()
+    t.drive(40)
+    print(t, t.get_fare())
+
+    sst = SilverServiceTaxi("Limo", 100, 2)
+    print(sst, sst.get_fare())
+    sst.drive(10)
+    print(sst, sst.get_fare())
+
+
 main()
