5e4834ab506e64de8ff654652dcd282d37f4eaa6
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-19 10:42:24 +0800

    error check, if choice = d, get distance_to_drive and print message

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -22,6 +22,16 @@ def main():
                 current_taxi = taxis[taxi_choice]
             except IndexError:
                 print("Invalid taxi choice")
+        elif menu_choice == "d":
+            if current_taxi:
+                current_taxi.start_fare()
+                distance_to_drive = float(input("Drive how far? "))
+                current_taxi.drive(distance_to_drive)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+                total_bill += trip_cost
+            else:
+                print("You need to choose a taxi before you can drive")
 
 
 def display_taxis(taxis):
