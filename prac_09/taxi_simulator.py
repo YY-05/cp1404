e12d0c2a3e010549b0bc11fa97359c299d09d940
Guo Yuye <yuye.guo@my.jcu.edu.au>
2024-11-19 10:34:45 +0800

    Write a taxi simulator program that uses your Taxi and SilverServiceTaxi     classes

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,3 +3,13 @@ from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    total_bill = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+
+
+main()
